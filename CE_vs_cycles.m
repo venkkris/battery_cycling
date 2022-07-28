33c7517f99ea1b1820f4e2d4d19ee3be38c66c23
Venkatesh Krishnamurthy <71044775+venkkris@users.noreply.github.com>
2022-07-28 16:49:28 -0400

    Minor aesthetic mods to CE pretty plot

--- CE_vs_cycles.m
+++ CE_vs_cycles.m
@@ -3,21 +3,22 @@ clc;
 close all;
 clear all;
 
-color = 'r';
+color = 'b';
 filename = 'pretty_plots/coulombic_efficiency.png';
 values = fscanf(fopen('main_out/coulombic_efficiencies.txt','r'),'%f');
 
 close all;
 figure()
-plot(values,'-o','Color', color, 'LineWidth', 3, 'MarkerSize', 15, 'HandleVisibility', 'off');
+plot(values,'-','Color', color, 'LineWidth', 3, 'MarkerSize', 15, 'HandleVisibility', 'off');
 hold on
 box on
+plot([0,length(values)],[100,100], '--', 'Color', 'k', 'LineWidth', 3, 'HandleVisibility', 'off');
 title('Coulombic efficiency vs Cycles');
 xlabel('Cycles');
 ylabel('Coulombic efficiency (%)');
-% xticks(0:2:18);
-% yticks(0:0.5:3);
-% xlim([0,17]);
+yticks(round(min(values)-2):3:round(max(values)+2));
+xlim([0,length(values)+1]);
+ylim([min(values)-2, max(values)+2]);
 
 set(gcf, 'Color', 'w');
 set(gca, 'FontName', 'Times New Roman', 'FontSize', 20, ... 
