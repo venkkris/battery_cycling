d9bae3823a499cbee6c0f404e29f6ee8a40f46fc
Venkatesh Krishnamurthy <71044775+venkkris@users.noreply.github.com>
2022-08-06 19:23:50 -0400

    Add dQ/dV data to plot.py; separate pplot script

--- plot.py
+++ plot.py
@@ -11,6 +11,7 @@ import cv2
 import numpy as np
 import glob
 import os
+from math import isclose
 from datetime import datetime
 
 
@@ -23,13 +24,28 @@ voltage_limits = [1.5, 4.8]      # Voltage limits for all plots
 remove_OCV_part = True           # Removes OCV part for plot of each cycle; not removed for time series plots
 stitch_files = True              # Stitches together multiple files into one dataframe; use when multiple files are part of one test
 
+dqdv_rel_tol = 0.01             # Relative tolerance for dQ/dV
+
 color1 = 'red'
 color2 = 'black'
+color_ch = '#0069c0'
+color_disch = '#0069c0'
+color_CE = '#0069c0'
+CE_100pc_line = True   # Add line at 100% Coulombic efficiency
+
+# Other colors
+# color = '#0047ab' # Cobalt blue
+# color = '#0b1d78' # Dark blue
+# color = '#332288' # Dark purple
+# color = '#0b1d78' # Dark blue
+# color = '#808080' # Gray
+# color = '#6b6b6b' # Dark gray
 
 ###############################################################################
 # Function definitions
 ###############################################################################
 
+# Helper functions- called by functions but not main
 def colorFader(c1,c2,mix=0):
     """Function to interpolate between two chosen colors.
     fade (linear interpolate) from color c1 (at mix=0) to c2 (mix=1)"""
@@ -53,19 +69,61 @@ def is_it_discharging(group):
         return False
 
 
+# Functions: Called by main
+def data_tailor():
+    """
+    Function to stitch data from multiple files into one dataframe.
+
+    In tests with multiple steps, e.g. EIS and GCPL, the Biologic inserts a tag
+    into each filename with the type of step. If there are multiple GCPL steps in
+    a given test protocol and stitch_files is True, this loop will concatenate them
+    into a single dataframe. If either condition is not met, it defaults to the
+    first .mpr file it finds.
+
+    Arguments:
+    None
+
+    Returns:
+    data: A Pandas dataframe instance
+    """
+    # Read data from mpr file into pandas dataframe object
+    gcpl_filelist = glob.glob('*_GCPL_*.mpr')   # List of files with 'GCPL' in filename
+    data = pd.DataFrame()   #Initialize empty dataframe
+
+    if len(gcpl_filelist) > 0 and stitch_files:
+        for filename in gcpl_filelist:
+            mpr_file = BioLogic.MPRfile(filename)
+            data = pd.concat([data, pd.DataFrame(mpr_file.data)],ignore_index=True)
+    else:
+        filename = glob.glob('*.mpr')[0]    # First .mpr file
+        mpr_file = BioLogic.MPRfile(filename)
+        data = pd.DataFrame(mpr_file.data)
+    print(str(datetime.now() - startTime)+' Read data.')
+    return data
+
+
 def plot_time_series(data, quantity, plot_name):
-    """Plots time series (quantity vs time in hours).
+    """
+    Plots time series (quantity vs time in hours).
 
     Arguments: 
     data = pandas dataframe object
     quantity = quantity to be plotted on y axis
-    plot_name = plot name"""
-    plt.figure()
+    plot_name = plot name
+
+    Returns:
+    None
+    """
+    os.makedirs('time_series',exist_ok=True)
+    plt.figure(figsize=(16,4))
+    mpl.rcParams['font.size'] = 16
     plt.plot(data['time/s']/3600, data[quantity])
+    plt.xlim([0, max(data['time/s'])/3600])
     plt.xlabel('Time (hr)')
     plt.ylabel(quantity)
-    plt.savefig(plot_name)
+    plt.savefig(plot_name, bbox_inches='tight')
     plt.close()
+    return None
 
 
 def plot_all_time_series(data):
@@ -88,26 +146,37 @@ def plot_all_time_series(data):
     plot_time_series(data=data, quantity='dQ/mA.h', plot_name='time_series/dQ.png')
     plot_time_series(data=data, quantity='Ns', plot_name='time_series/Ns.png')
     plot_time_series(data=data, quantity='half cycle', plot_name='time_series/half_cycle.png')
-    print('Plotted all time series data.')
+    print(str(datetime.now() - startTime)+' Plotted all time series data.')
+    return None
 
 
 def plot_voltage_capacity_ref_initial(data):
-    """Function to plot voltage vs capacity referenced to initial capacity.
+    """
+    Function to plot voltage vs capacity referenced to initial capacity.
     
     Argument: 
-    data = pandas dataframe object"""
-    plt.figure()
+    data = pandas dataframe object
+
+    Returns:
+    None
+    """
+
+    os.makedirs('main_out',exist_ok=True)
+    plt.figure(figsize=(16,4))
     plt.plot(-1*data['(Q-Qo)/mA.h'], data['Ewe/V'], '-')
+    plt.xlim([min(-1*data['(Q-Qo)/mA.h']), max(-1*data['(Q-Qo)/mA.h'])])
     plt.xlabel('Capacity (mAh)')
     plt.ylabel('Voltage (V)')
     plt.ylim(voltage_limits)
-    plt.savefig('main_out/voltage_vs_capacity.png')
+    plt.savefig('main_out/voltage_vs_capacity.png', bbox_inches='tight')
     plt.close()
-    print('Plotted voltage vs capacity referenced to initial capacity.')
+    print(str(datetime.now() - startTime)+' Plotted voltage vs capacity ref. initial.')
+    return None
 
 
 def plot_capacity_vs_cycle(data):
-    """Function to plot charge/discharge capacity vs cycles.
+    """
+    Function to plot charge/discharge capacity vs cycles.
     Also plots Coulombic efficiency vs cycles.
 
     Argument:
@@ -117,6 +186,7 @@ def plot_capacity_vs_cycle(data):
     disharge capacity (array), charge capacity (array)
     """
 
+    os.makedirs('main_out',exist_ok=True)
     grouped = data.groupby("half cycle", sort=False)
     disch_capacity = []
     ch_capacity = []
@@ -134,28 +204,40 @@ def plot_capacity_vs_cycle(data):
     # Plot Discharge capacity vs cycles
     plt.figure()
     plt.plot(np.arange(start=1, stop=len(disch_capacity)+1, step=1), 
-    disch_capacity, '-o')
-    plt.xlabel('Number of cycles')
-    plt.ylabel('Discharge capacity (mAh)')
-    plt.xlim(1, len(disch_capacity)+1)
+    disch_capacity, linestyle='solid', color=color_disch, linewidth=3, markersize=15)
+    plt.xlabel('Number of cycles', 
+    fontweight='bold', fontname='Times New Roman', fontsize=20)
+    plt.ylabel('Discharge capacity (mAh)', 
+    fontweight='bold', fontname='Times New Roman', fontsize=20)
+    plt.title('Discharge capacity vs cycles', 
+    fontweight='bold', fontname='Times New Roman', fontsize=20)
+    plt.xlim(0, len(disch_capacity)+1)
     plt.ylim(0, 1.05*max(disch_capacity))
-    plt.title('Discharge capacity vs Number of cycles')
-    plt.savefig('main_out/discharge_capacity_vs_cycles.png')
+    plt.minorticks_on()
+    plt.tick_params(axis='both', which='minor', length=4, width=1)
+    plt.tick_params(axis='both', which='major', labelsize=14, length=7, width=1.5)
+    plt.savefig('main_out/discharge_capacity_vs_cycles.png', bbox_inches='tight', dpi=300)
     plt.close()
 
 
     # Plot Charge capacity vs cycles
     plt.figure()
     plt.plot(np.arange(start=1, stop=len(ch_capacity)+1, step=1), 
-    ch_capacity, '-o')
-    plt.xlabel('Number of cycles')
-    plt.ylabel('Charge capacity (mAh)')
-    plt.xlim(1, len(ch_capacity)+1)
+    ch_capacity, linestyle='solid', color=color_ch, linewidth=3, markersize=15)
+    plt.xlabel('Number of cycles',
+    fontweight='bold', fontname='Times New Roman', fontsize=20)
+    plt.ylabel('Charge capacity (mAh)',
+    fontweight='bold', fontname='Times New Roman', fontsize=20)
+    plt.title('Charge capacity vs cycles',
+    fontweight='bold', fontname='Times New Roman', fontsize=20)
+    plt.xlim(0, len(ch_capacity)+1)
     plt.ylim(0, 1.05*max(ch_capacity))
-    plt.title('Charge capacity vs Number of cycles')
-    plt.savefig('main_out/charge_capacity_vs_cycles.png')
+    plt.minorticks_on()
+    plt.tick_params(axis='both', which='minor', length=4, width=1)
+    plt.tick_params(axis='both', which='major', labelsize=14, length=7, width=1.5)
+    plt.savefig('main_out/charge_capacity_vs_cycles.png', bbox_inches='tight', dpi=300)
     plt.close()
-    print('Plotted charge/discharge capacity vs cycles.')
+    print(str(datetime.now() - startTime)+' Plotted charge/discharge capacity vs cycles.')
 
 
     # Plot Coulombic efficiency vs cycles
@@ -165,17 +247,29 @@ def plot_capacity_vs_cycle(data):
     y = np.multiply(100, y)
 
     plt.figure()
-    plt.plot(x, y, '-o')
-    plt.plot(x, [100]*len(x), '-')   # Plot 100% line
-    plt.xlabel('Number of cycles')
-    plt.ylabel('Coulombic efficiency (%)')
-    plt.xlim(-1, num_cycles+2)
-    plt.ylim(min(y)-2, max(y)+2)
-    plt.title('Coulombic efficiency vs Number of cycles')
-    plt.savefig('main_out/coulombic_efficiency_vs_cycles.png')
+    plt.plot(x, y, linestyle='solid', color=color_CE, linewidth=3, markersize=15)
+    # Plot 100% line
+    if CE_100pc_line:
+        plt.plot(x, [100]*len(x), 
+        linestyle='dashed', color='k', linewidth=3, markersize=15) 
+        plt.ylim(min(*y,100)-2, max(*y, 100)+2)
+    else:
+        plt.ylim(min(y)-2, max(y)+2)
+    plt.xlim(1, num_cycles)
+
+    plt.xlabel('Number of cycles',
+    fontweight='bold', fontname='Times New Roman', fontsize=20)
+    plt.ylabel('Coulombic efficiency (%)',
+    fontweight='bold', fontname='Times New Roman', fontsize=20)
+    plt.title('Coulombic efficiency vs cycles',
+    fontweight='bold', fontname='Times New Roman', fontsize=20)
+    plt.minorticks_on()
+    plt.tick_params(axis='both', which='minor', length=4, width=1)
+    plt.tick_params(axis='both', which='major', labelsize=14, length=7, width=1.5)
+    plt.savefig('main_out/coulombic_efficiency_vs_cycles.png', bbox_inches='tight', dpi=300)
     plt.close()
     np.savetxt('main_out/coulombic_efficiencies.txt', np.array(y))
-    print('Plotted Coulombic efficiency vs cycles.')
+    print(str(datetime.now() - startTime)+' Plotted Coulombic efficiency vs cycles.')
 
     return disch_capacity, ch_capacity
 
@@ -192,6 +286,8 @@ def plot_charge_discharge_profiles(data, disch_capacity, ch_capacity):
     Note:
     arrays of ch. and disch. capacities are returned by plot_capacity_vs_cycle
     """
+
+    os.makedirs('cycles',exist_ok=True)
     disch_images = []   # Array of discharge images
     ch_images = []      # Array of charge images
     disch = 1
@@ -303,8 +399,42 @@ def plot_charge_discharge_profiles(data, disch_capacity, ch_capacity):
     plt.ylim(voltage_limits)
     plt.savefig('main_out/combined_charge_profiles.png')
     plt.close()
-    print('Plotted charge/discharge profiles.')
+    print(str(datetime.now() - startTime)+' Plotted charge/discharge profiles.')
 
+
+def save_dQ_dV_data(ch_capacity, disch_capacity):
+    """
+    Function reads from cycles/*_i.csv files and saves to cycles/*_i_dQdV.csv
+    where * = 'charge' or 'discharge' and i is integer representing cycle number.
+    Uses ch_capacity, disch_capacity to get filenames.
+    Columns: voltage, dQ/dV
+    """
+    filenames = []
+    for i in range(len(ch_capacity)):
+        filenames.append('cycles/' + 'charge_' + str(i+1) + '.csv')
+    for i in range(len(disch_capacity)):
+        filenames.append('cycles/' + 'discharge_' + str(i+1) + '.csv')
+
+
+    for filename in filenames:
+        data = pd.read_csv(filename, names=['charge', 'voltage'])
+
+        # Filter data: Remove point if voltage is the same as previous to dqdv_rel_tol V
+        for i in range(1, len(data)):
+            if isclose(data['voltage'][i], data['voltage'][i-1], rel_tol=dqdv_rel_tol):
+                data.drop(i)
+        q, v = data['charge'].values, data['voltage'].values
+
+        # Compute dQ/dV using centered finite difference
+        dqdv = [0]
+        for i in range(1, len(q)-1):
+            dqdv.append( (q[i+1] - q[i-1])/(v[i+1]-v[i-1]) )
+        dqdv.append(0)
+        # Save to file
+        np.savetxt(filename[:-4] + '_dQdV.csv', np.column_stack((v, dqdv)), delimiter=',')
+    
+    print(str(datetime.now() - startTime)+' Saved dQ/dV data.')
+    return None
 
 
 
@@ -313,55 +443,29 @@ def plot_charge_discharge_profiles(data, disch_capacity, ch_capacity):
 # Main
 ###############################################################################
 startTime = datetime.now()
+print(str(datetime.now() - startTime)+' Started execution.')
 
 # Make directories for plots
-os.makedirs('time_series',exist_ok=True)
-os.makedirs('cycles',exist_ok=True)
-os.makedirs('main_out',exist_ok=True)
 os.makedirs('pretty_plots/',exist_ok=True)
 
-
-# Read data from mpr file into pandas dataframe object
-gcpl_filelist = glob.glob('*_GCPL_*.mpr')   # List of files with 'GCPL' in filename
-data = pd.DataFrame()   #Initialize empty dataframe
-"""
-In tests with multiple steps, e.g. EIS and GCPL, the Biologic inserts a tag
-into each filename with the type of step. If there are multiple GCPL steps in
-a given test protocol and stitch_files is True, this loop will concatenate them
-into a single dataframe. If either condition is not met, it defaults to the
-first .mpr file it finds.
-"""
-if len(gcpl_filelist) > 0 and stitch_files:
-    for filename in gcpl_filelist:
-        mpr_file = BioLogic.MPRfile(filename)
-        data = pd.concat([data, pd.DataFrame(mpr_file.data)],ignore_index=True)
-else:
-    filename = glob.glob('*.mpr')[0]    # First .mpr file
-    mpr_file = BioLogic.MPRfile(filename)
-    data = pd.DataFrame(mpr_file.data)
-
-
-# Plot all imp. time series data
-plot_all_time_series(data)
-
-
-# Plot voltage vs capacity referenced to initial capacity
-plot_voltage_capacity_ref_initial(data)
-
+data = data_tailor()
+plot_all_time_series(data)   
+# plot_voltage_capacity_ref_initial(data)
 
 # Remove OCV part for all cycles
 if remove_OCV_part:
     data.drop(data[data['dQ/mA.h'] == 0].index, inplace=True)
 
-
 # Plot charge/discharge capacity vs cycles
 disch_capacity, ch_capacity = plot_capacity_vs_cycle(data)
-
 
 # Plot charge/discharge profiles
 plot_charge_discharge_profiles(data, disch_capacity, ch_capacity)
 
+# Save dQ/dV data
+save_dQ_dV_data(ch_capacity, disch_capacity)
 
-print("Finished execution in %s."  % (datetime.now() - startTime) )
+
+print("%s Finished execution."  % (datetime.now() - startTime) )
 ###############################################################################
 
---
+++ pp_dqdv.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
+def colorFader(c1,c2,mix=0):
+    """Function to interpolate between two chosen colors.
+    fade (linear interpolate) from color c1 (at mix=0) to c2 (mix=1)"""
+    c1=np.array(mpl.colors.to_rgb(c1))
+    c2=np.array(mpl.colors.to_rgb(c2))
+    return mpl.colors.to_hex((1-mix)*c1 + mix*c2)
+
+
+def smoothen_dqdv(data, smooth_type):
+    """
+    Smoothen the dQ/dV data.
+    Currently implements rolling average of 3 and Savitzky-Golay.
+
+    Arguments:
+    data -- dataframe with V, dQ/dV as columns
+    smooth_type -- string, either 'savitzky-golay' or 'rolling'
+
+    Returns:
+    data -- dataframe with V and smoothed dQ/dV as columns.
+    """
+
+    if smooth_type == 'rolling':
+        data['dqdv'] = data['dqdv'].rolling(3).mean()
+
+    elif smooth_type == 'savitzky_golay':
+        raise NotImplementedError
+
+    return data
+
+
+###############################################################################
+cycle_nums = [60, 65, 70]
+voltage_limits = [0, 3]         # x limits
+smooth_type = 'rolling'           # 'None', 'rolling' or 'savitzky-golay'
+
+dqdv_color1 = '#53a4ec'
+dqdv_color2 = '#092d4d'
+###############################################################################
+
+
+for index, cycle_num in enumerate(cycle_nums):
+    color = colorFader(dqdv_color1, dqdv_color2, mix=index/len(cycle_nums))
+    # Charge data
+    ch_data = pd.read_csv('cycles/charge_'+str(cycle_num)+'_dQdV.csv', 
+    names=['voltage', 'dqdv'])
+    ch_data = smoothen_dqdv(ch_data, smooth_type)
+    plt.plot(ch_data['voltage'], ch_data['dqdv'], label='Cycle '+str(cycle_num),
+    linestyle='solid', color=color, linewidth=3, markersize=15)
+
+    # Horizontal line at 0
+    plt.axhline(y=0, color='black', linestyle='dashed', linewidth=1.5)
+
+    # Discharge data
+    disch_data = pd.read_csv('cycles/discharge_'+str(cycle_num)+'_dQdV.csv', 
+    names=['voltage', 'dqdv'])
+    disch_data = smoothen_dqdv(disch_data, smooth_type)
+    plt.plot(disch_data['voltage'], disch_data['dqdv'],
+    linestyle='solid', color=color, linewidth=3, markersize=15)
+
+
+plt.xlabel('Voltage (V)',
+fontweight='bold', fontname='Times New Roman', fontsize=20)
+plt.ylabel('dq/dV (mAh/V)',
+fontweight='bold', fontname='Times New Roman', fontsize=20)
+plt.title('Differential capacity',
+fontweight='bold', fontname='Times New Roman', fontsize=20)
+plt.legend(loc='upper right', fontsize=15)
+
+plt.xlim(voltage_limits)
+plt.minorticks_on()
+plt.tick_params(axis='both', which='minor', length=4, width=1)
+plt.tick_params(axis='both', which='major', labelsize=14, length=7, width=1.5)
+
+plt.savefig('pretty_plots/dQdV.png', dpi=300, bbox_inches='tight')
+plt.close()
