b7331ec7867ae1977fbd7df4fd7d2b622f94391d
Venkatesh Krishnamurthy <71044775+venkkris@users.noreply.github.com>
2022-08-10 01:45:01 -0400

    Added testing prior to execution in batch_exe.py

--- batch_exe.py
+++ batch_exe.py
@@ -10,15 +10,28 @@ Instructions for use:
 import os
 import glob
 
-path_to_rebecca = '~/Desktop/battery_cycling/'
+path_to_rebecca = '/Users/venkatesh/Desktop/battery_cycling/'
 
-mpr_files = glob.glob('*.mpr')
-for mpr_file in mpr_files:
-    print('Processing {}.'.format(mpr_file))
-    os.system('mkdir ' + mpr_file[:-4])
-    os.system('mv ' + mpr_file + ' ' + mpr_file[:-4])
-    os.system('cp ' + path_to_rebecca + '*.py ' + mpr_file[:-4])
-    os.chdir(mpr_file[:-4])
-    os.system('python plot.py')
-    os.chdir('..')
-    print('Done.')
+# Test if path_to_rebecca is correct
+if not os.path.isdir(path_to_rebecca):
+    print('Error: path_to_rebecca is not a directory.')
+    exit()
+print('Path to REBECCA: ' + path_to_rebecca)
+print('\nFiles in REBECCA: ' + str(os.listdir(path_to_rebecca)))
+print('\nDoes this look correct? (y/n):')
+answer = input()
+
+if answer == 'y':
+    mpr_files = glob.glob('*.mpr')
+    for mpr_file in mpr_files:
+        print('Processing {}.'.format(mpr_file))
+        os.system('mkdir ' + mpr_file[:-4])
+        os.system('mv ' + mpr_file + ' ' + mpr_file[:-4])
+        os.system('cp ' + path_to_rebecca + '*.py ' + mpr_file[:-4])
+        os.chdir(mpr_file[:-4])
+        os.system('python plot.py')
+        os.chdir('..')
+        print('Done.')
+else:
+    print('Exiting.')
+    exit()
